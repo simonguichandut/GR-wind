240951d8afa25a06d3e005fad759baacaea443aa
Simon Guichandut <simon@vlan123-250.physics.mcgill.ca>
2019-04-29 23:43:56 -0400

    monday night

--- output_data.py
+++ output_data.py
@@ -8,9 +8,19 @@ def write_to_file(setup,logMdot,data):
         os.mkdir('out')
 
     filename = '_'.join([str(setup[i]) for i in range(len(setup))]) + '_' + str(logMdot)
+    path = 'out/'+filename
 
-    with open('out/'+filename,'w') as f:
-        f.write('r(km) \t u(km/s) cs(km/s) \t rho(g/cm3) \t T(K) \t P(dyne/cm2) \t phi \t L*(erg/s) \t L(erg/s) \t LEddloc(erg/s) \t E(erg)')
+    # if os.path.exists(path):
+    #     c = input('File exists, proceed?')
+    c=1
+    if c:
+        with open(path,'w') as f:
+            f.write('r(km) \t u(km/s) cs(km/s) \t rho(g/cm3) \t T(K) \t P(dyne/cm2) \t phi \t L(erg/s) \t L*(erg/s) \t E(erg)')
+
+            R, T, Rho, u, Phi, Lstar, L, E, P, cs, tau = data
+            for i in range(len(R)):
+                f.write('%0.3e \t %0.3e \t %0.3e \t %0.3e \t %0.3e \t %0.3e \t %0.3e \t %0.3e \t %0.3e \t %0.3e'%
+                    (R[i]/1e5 , u[i]/1e5 , cs[i]/1e5 , Rho[i] , T[i] , P[i] , Phi[i] , L[i] , Lstar[i] , E[i]))
 
     
-write_to_file(['He',1.4,10,3,4],18,1)
+# write_to_file(['He',1.4,10,3,4],18,1)
