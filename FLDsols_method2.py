bc678c18dcf4c0b555193dac25aa1d6f8690447e
Simon Guichandut <simonguichandut@gmail.com>
2020-02-11 13:11:05 -0500

    found one FLD solution! onto making gridsearch automatic

--- FLDsols_method2.py
+++ FLDsols_method2.py
@@ -168,6 +168,8 @@ def innerIntegration_inf():
 
 #%% Evaluate crossings
 
+from scipy.optimize import fminbound
+
 def FindCrossing(x1,y1,x2,y2): # Finds if there is a crossing between two lines and if so returns an interpolated location
     
     # need increasing values for spline fit
@@ -191,9 +193,17 @@ def FindCrossing(x1,y1,x2,y2): # Finds if there is a crossing between two lines 
     
     
     f1,f2 = IUS(x1,y1),IUS(x2,y2) # spline fits
+
     if (f2(x[0])-f1(x[0]))*(f2(x[-1])-f1(x[-1])) > 0:
+        diff = lambda xi : abs(f2(xi)-f1(xi))
 #        print('no crossing')
-        return (False,)
+    
+        # if no crossing, take "crossing" as where the curves get closest to each other
+        minimum = fminbound(diff,x[0],x[1],full_output=True,disp=False)
+#        print(minimum)
+    
+        return (False,minimum)
+
 
     else:
         diff = lambda xi : f2(xi)-f1(xi)
@@ -245,13 +255,13 @@ def MakeWindFLD(params, logMdot, Verbose=0): # this would go into the full MakeW
     if (not Tcross[0]) or (not ucross[0]):  # no crossing
         return +1000
     else:
-        return Tcross[1] - ucross[1]
+        return ucross[1] - Tcross[1]
 
 
 
 # Parameters
-#logMdot = 18.5
-#params = [1.025,7.2,8.42025]           # Edot/LEdd, logTs, logvinf
+logMdot = 18.5
+params = [1.025,7.2,8.42025]           # Edot/LEdd, logTs, logvinf
 
 #params = [1.025,7.2,8.4]
 #params = [1.025,6.9,8]
@@ -265,16 +275,27 @@ def MakeWindFLD(params, logMdot, Verbose=0): # this would go into the full MakeW
 import pickle
 def get_map(logMDOT,Edot):
 
-    n=10
-    Errors = [[] for i in range(n)] 
-    Tsvals = np.linspace(7.1,7.3,n)
-    vinfvals = np.linspace(8.2,8.7,n)
+#    n=10
+#    Tsvals = np.linspace(7.1,7.3,n)
+#    vinfvals = np.linspace(8.2,8.7,n)
     
+#    n=10
+#    Tsvals = np.linspace(7.15,7.25,n)
+#    vinfvals = np.linspace(8.3,8.5,n)
+    
+    n=10
+    Tsvals = np.linspace(7.16,7.22,n)
+    vinfvals = np.linspace(8.4,8.45,n)
+    
+    Errors = [[] for i in range(n)] 
     for i,Ts in zip(np.arange(n),Tsvals):
         print('\n\n LOG TS = %.3f \n\n'%Ts)
         for vinf in vinfvals:
             try:
                 z = MakeWindFLD([Edot,Ts,vinf],logMDOT,Verbose=0)
+                
+#                if z!=1000: z /= rs
+                
                 print('log(vinf) = %.3f : %.3e'%(vinf,z))
             except:
 #                pass
@@ -285,7 +306,7 @@ def get_map(logMDOT,Edot):
             Errors[i].append(z)
             
 
-    filename = 'analysis/errorspaces_FLD/save'+str(logMDOT)+'_'+str(Edot/LEdd)+'.p'
+    filename = 'analysis/errorspaces_FLD/save'+str(logMDOT)+'_'+str(Edot)+'.p'
     with open(filename,'wb') as f:
         pickle.dump([Tsvals,vinfvals,Errors],f)
         
@@ -293,186 +314,164 @@ def get_map(logMDOT,Edot):
     
 #
 #    print('\n\nFINISHED \nMap file saved to ',filename)    
-            
-            
-map1 = get_map(18.5,1.025)
+    
 
+import matplotlib.colors as colors
 
-#%% PLOT MAP
-#
-#
-#def plot_map(map):
-#
-#    filename = 'analysis/errorspaces/save'+str(logMDOT)+'.p'
-#    [Edotvals,Tsvals,Errors]=pickle.load(open(filename,'rb'))
-#
-#    fig,(ax1,ax2) = plt.subplots(1,2,figsize=set_size('mnras1col'))    
-#    fig.subplots_adjust(wspace=0.25)
-#    ax1.patch.set_color('.25')
-#    ax2.patch.set_color('.25')
-#
-#    # Option 1 
-##     cmap = plt.get_cmap('RdBu') # diverging colormap (white at center)
-##     levels=np.arange(-1,1,0.01)
-##     im1 = ax1.contourf(Edotvals,Tsvals,array(Errors[0]),levels=levels,cmap=cmap)
-##     im2 = ax2.contourf(Edotvals,Tsvals,array(Errors[1]),levels=levels,cmap=cmap)
-##     fig.colorbar(im1,ax=ax1)
-##     fig.colorbar(im2,ax=ax2)
-##     ax1.set_title(r'Error #1 : ($L_{phot}-4\pi r^2\sigma T^4$)',fontsize=15)
-##     ax2.set_title(r'Error #2 : ($R_{base}-R_{NS}$)',fontsize=15)    
-#
-#    # Option 2
-#    cmap = plt.get_cmap('YlOrRd') # diverging colormap (white at bottom)
-#    levs=np.logspace(-3,1,50)
-#    im1 = ax1.contourf(Edotvals,Tsvals,np.abs(array(Errors[0])),levs,norm=colors.LogNorm(),cmap=cmap)
-#    im2 = ax2.contourf(Edotvals,Tsvals,np.abs(array(Errors[1])),levs,norm=colors.LogNorm(),cmap=cmap)
-#    fig.colorbar(im1,ax=ax1,ticks=[1e-3, 1e-2, 1e-1, 1e0, 1e1])
-#    fig.colorbar(im2,ax=ax2,ticks=[1e-3, 1e-2, 1e-1, 1e0, 1e1])
-#    ax1.set_title(r'Error 1 : $\vert L_{ph}-4\pi r^2\sigma T^4\vert$/$L_{ph}$')
-#    ax2.set_title(r'Error 2 : $\vert r_{b}-R_{NS}\vert$/$R_{NS}$')    
-#
-#
-#    ax1.set_xlabel(r'$\dot{E}/L_{Edd}$')
-#    ax2.set_xlabel(r'$\dot{E}/L_{Edd}$')
-#    ax1.set_ylabel(r'log $T_s$ (K)')
-#
-#                
-#    ax1.set_xlim([Edotvals[0],Edotvals[-1]])
-#    ax1.set_ylim([Tsvals[0],Tsvals[-1]])  
-#    ax2.set_xlim([Edotvals[0],Edotvals[-1]])
-#    ax2.set_ylim([Tsvals[0],Tsvals[-1]])     
-#    
-#    
-#    # Add scatters for errors
-#    cases_left = (r'Mach $\rightarrow 1$',r'$\tau^*_{min}>3$','?','tau^*(r_c)<3')
-#    cases_right = ('u=0','Diverging (NaN)','?')
-#    color_left = ('m','c','w')
-#    color_right = ('r','g','k')
-#    legendleft,legendright = False,False
-#    
-#    for E,caseL,caseR,cl,cr in zip((100,200,300,400),cases_left,cases_right,color_left,color_right):
-#    
-#        # Left plot (error on outer int)
-#        Epointsleft,Tspointsleft = [],[]
-#        for i in range(len(Edotvals)):
-#            for j in range(len(Tsvals)):
-#                if Errors[0][j][i]==E:
-#                    Epointsleft.append(Edotvals[i])
-#                    Tspointsleft.append(Tsvals[j])
-#        
-#        if len(Epointsleft)>0:
-#            ax1.scatter(Epointsleft,Tspointsleft,s=3,label=caseL,color=cl)
-#            legendleft=True
-#            
-#        
-#        # Right plot (error on inner int)
-#        Epointsright,Tspointsright = [],[]
-#        for i in range(len(Edotvals)):
-#            for j in range(len(Tsvals)):
-#                if Errors[1][j][i]==E:
-#                    Epointsright.append(Edotvals[i])
-#                    Tspointsright.append(Tsvals[j])
-#        
-#        if len(Epointsright)>0:
-#            ax2.scatter(Epointsright,Tspointsright,s=3,label=caseR,color=cr)
-#            legendright=True
-#    
-#    
-#    if legendleft: ax1.legend(loc=4)
-#    if legendright: ax2.legend(loc=4)
-#        
-#    
-#    # plt.tight_layout()
-#    
-#    filename = str(logMDOT).replace('.','_') # dont want points in filenames
-#    fig.savefig('analysis/errorspaces/'+filename+'.'+img)    
-#
-#            
-#            
+def plot_map(logMDOT,Edot):
+
+    filename = 'analysis/errorspaces_FLD/save'+str(logMDOT)+'_'+str(Edot/LEdd)+'.p'
+    [Tsvals,vinfvals,E]=pickle.load(open(filename,'rb'))
+
+    fig,ax = plt.subplots(1,1)    
+    fig.subplots_adjust(wspace=0.25)
+    ax.patch.set_color('.25')      
+    cmap = plt.get_cmap('Blues') 
+    
+    for i in range(len(E)):
+        for j in range(len(E[0])):
+            if E[i][j]==1000:
+                E[i][j]=1           # ignore these values (1 will be out of range)
+    levs=np.linspace(5,9,10)
+    im = ax.contourf(vinfvals,Tsvals,np.log10(np.abs(E)),levs,cmap=cmap)
+    
+#    levs=np.linspace(0,20,20)
+#    im = ax.contourf(vinfvals,Tsvals,E,levs,cmap=cmap)    
+    
+    fig.colorbar(im,ax=ax)
+    ax.set_title(r'Error in rmatch')
+
+    ax.set_xlabel(r'log $v_\infty$')
+    ax.set_ylabel(r'log $T_s$ (K)')
+
+    
+    return Tsvals,vinfvals,E,fig,ax
+    
+
+        
+#map1 = get_map(18.5,1.025)
+#Tsvals,vinfvals,E,fig,ax = plot_map(18.5,1.025*LEdd)
+
+   
             
 #%% Manual 
             
-#            
-#
-#logMdot = 18.5
-#
-##params = [1.025,7.2,8.42]             
+            
+
+logMdot = 18.5
+
+#params = [1.025,7.2,8.42]             
 #params =  [1.025,7,8]
-#
-#
-#global Mdot,Edot,Ts,verbose,rs,vinf
-#Mdot, Edot, Ts, verbose = W.setup_globals(params[:2],logMdot,Verbose=1,return_them=True)
-#vinf = 10**params[2]
-#
-#
-#rs = W.rSonic(Ts)
-#res1 = W.outerIntegration(returnResult=True)
-#r1 = res1.t
-#T1,phi1 = res1.sol(r1)
-#u1, _,_,_ = W.calculateVars_phi(r1,T1, phi1, subsonic=False)
-#
-#
-#gammainf = gamma(vinf)
-#Linf = Edot + Mdot*c**2*(1-gammainf)
-#r0 = 1e12
-#   
-## Need to rootfind for the values of v0 and Lstar0
-#
-#def calc_initial_vars(x):
-#    v0,Lstar0 = x
-#    L0 = Lcomoving(Lstar0,r0,v0)
-#    rho0 = Mdot/(4*pi*r0**2*v0*Y(r0,v0))
-#    T0 = (L0/(4*pi*r0**2*arad*c))**(0.25)
-#    return v0,Lstar0,L0,rho0,T0
-#
-#def error(x):
-#    
-#    v0,Lstar0,L0,rho0,T0 = calc_initial_vars(x)
-#    
-#    E1 = Y(r0,v0) - gammainf + eos.kappa(rho0,T0)*L0/(4*pi*r0*c**3*Y(r0,v0))   # Integrated momentum equation ignoring Pg'
-#    E2 = (Edot + Mdot*c**2 - Lstar0 - Mdot*eos.H(rho0,T0)*Y(r0,v0))/LEdd
-#    return [E1,E2]
-#
-#x = fsolve(error,x0=[vinf,Linf])
-#v0,Lstar0,L0,rho0,T0 = calc_initial_vars(x)
-#
-#
-#def causality(r,y):
-#    T, v = y[:2]
-#    rho, L, _ = calculateVars_v(r, T, v=v)
-#    flux = L/(4*pi*r**2)
-#    Er = arad*T**4
-#    return flux-c*Er
-##causality.terminal = True
-#
-#    
-##def divergence(r,y):
-##    _,dv_dr = dr_fld(r,y)
-##    return dv_dr
-##divergence.direction = -1  # if both gradients have the same sign, the integration will diverge
-##divergence.terminal = True
-#
+
+#params = [1.025,7.12,8.405] # a good one : log10(E)=6.737
+
+#params = [1.025,7.1595,8.4137]    # best one so far : log10(E)=6.3072
+
+#params = [1.025,7.1595,8.414]   # this one is interesting to look at and maybe useful in future! shows the upper limit case of vinf i think
+
+
+#params = [1.025,7.1595,8.41374983]
+
+params = [1.025,7.175,8.41374983]    # wow!!
+
+#params = [1.025,7.175,8.413]   
+
+
+
+global Mdot,Edot,Ts,verbose,rs,vinf
+Mdot, Edot, Ts, verbose = W.setup_globals(params[:2],logMdot,Verbose=1,return_them=True)
+vinf = 10**params[2]
+
+
+rs = W.rSonic(Ts)
+res1 = W.outerIntegration(returnResult=True)
+r1 = res1.t
+T1,phi1 = res1.sol(r1)
+u1, _,_,_ = W.calculateVars_phi(r1,T1, phi1, subsonic=False)
+
+
+gammainf = gamma(vinf)
+Linf = Edot + Mdot*c**2*(1-gammainf)
+r0 = 1e12
+   
+# Need to rootfind for the values of v0 and Lstar0
+
+def calc_initial_vars(x):
+    v0,Lstar0 = x
+    L0 = Lcomoving(Lstar0,r0,v0)
+    rho0 = Mdot/(4*pi*r0**2*v0*Y(r0,v0))
+    T0 = (L0/(4*pi*r0**2*arad*c))**(0.25)
+    return v0,Lstar0,L0,rho0,T0
+
+def error(x):
+    
+    v0,Lstar0,L0,rho0,T0 = calc_initial_vars(x)
+    
+    E1 = Y(r0,v0) - gammainf + eos.kappa(rho0,T0)*L0/(4*pi*r0*c**3*Y(r0,v0))   # Integrated momentum equation ignoring Pg'
+    E2 = (Edot + Mdot*c**2 - Lstar0 - Mdot*eos.H(rho0,T0)*Y(r0,v0))/LEdd
+    return [E1,E2]
+
+x = fsolve(error,x0=[vinf,Linf])
+v0,Lstar0,L0,rho0,T0 = calc_initial_vars(x)
+
+
+def causality(r,y):
+    T, v = y[:2]
+    rho, L, _ = calculateVars_v(r, T, v=v)
+    flux = L/(4*pi*r**2)
+    Er = arad*T**4
+    return flux-c*Er
+#causality.terminal = True
+
+    
 #def divergence(r,y):
-#    return y[1]-vinf
-##    divergence.direction = -1  # velocity gradient becomes negative (v goes up as we go in, we're diverging for sure)
+#    _,dv_dr = dr_fld(r,y)
+#    return dv_dr
+#divergence.direction = -1  # if both gradients have the same sign, the integration will diverge
 #divergence.terminal = True
-#
-#
-#def hit_mach1(r,y): 
-#    cs = sqrt(eos.cs2(y[0]))
-#    return y[1]-cs
-#hit_mach1.terminal = True # stop integrating at this point
-#   
-#
-#        
-#   
-#rspan = (r0,2*rs)
-#result2 = solve_ivp(dr_fld, rspan, (T0,v0), method='RK45', dense_output=True, events=(causality,divergence,hit_mach1), rtol=1e-6)    
-#r2,(T2,u2)=result2.t,result2.y
-#
-#
-#fig,(ax1,ax2)=plt.subplots(1,2)
-#ax1.loglog(r1,T1)
-#ax2.loglog(r1,u1)
-#ax1.loglog(r2,T2)
-#ax2.loglog(r2,u2)
+
+def divergence(r,y):
+    return y[1]-vinf
+#    divergence.direction = -1  # velocity gradient becomes negative (v goes up as we go in, we're diverging for sure)
+divergence.terminal = True
+
+
+def hit_mach1(r,y): 
+    cs = sqrt(eos.cs2(y[0]))
+    return y[1]-cs
+hit_mach1.terminal = True # stop integrating at this point
+   
+
+
+rspan = (r0,rs)
+result2 = solve_ivp(dr_fld, rspan, (T0,v0), method='RK45', dense_output=True, events=(causality,divergence,hit_mach1), rtol=1e-6)    
+r2,(T2,u2)=result2.t,result2.y
+
+
+
+Tcross = FindCrossing(r1,T1,r2,T2)
+ucross = FindCrossing(r1,u1,r2,u2)    
+if (not Tcross[0]) or (not ucross[0]):  # no crossing
+    print('no crossing, error in radius of closest approach:',log10(abs(ucross[1][0]-Tcross[1][0])))
+    print('Magnitude of differences:',log10(Tcross[1][1]),log10(ucross[1][1]))
+else:
+    print(log10(ucross[1] - Tcross[1]))
+
+
+fig,(ax1,ax2)=plt.subplots(1,2,figsize=(12,7))
+ax1.loglog(r1,T1,label='from sonic point')
+ax2.loglog(r1,u1)
+ax1.loglog(r2,T2,label='from infinity')
+ax2.loglog(r2,u2) 
+ax1.set_xlabel('r (cm)')
+ax2.set_xlabel('r (cm)')
+ax1.set_ylabel('T (K)')
+ax2.set_ylabel('u (cm/s)')
+ax1.legend()
+plt.tight_layout()
+plt.show()
+
+
+
+
+
